01cd03862f1def5a9d88e802bd148c6453d41637
ederaparecidoleite <ederaparecidoleite@hotmail.com>
2023-05-20 21:59:23 -0300

    vercel

--- src/controllers/users/index.ts
+++ src/controllers/users/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
-import bcryptjs from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import Users from '../../models/users/index';
 import { IUsers } from '../../models/users/index';
@@ -7,9 +7,7 @@ import moment from 'moment';
 import Joi from 'joi';
 import signUpSchema from '../../validations/users/index';
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-
-// Register
+//register
 router.post('/auth/user/sign-up', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { info, security } = req.body;
@@ -18,12 +16,12 @@ router.post('/auth/user/sign-up', async (req: Request, res: Response, next: Next
         // Validate input data
         await signUpSchema.validateAsync({ ...info, ...security }, { abortEarly: false });
         // Create password hash
-        const salt = bcrypt.genSaltSync(10);
-        const passwordHash = bcrypt.hashSync(password, salt);
+        const salt = await bcrypt.genSalt(12);
+        const passwordHash = await bcrypt.hash(password, salt);
         // Get current date/time in Brazil timezone
         const data = new Date();
         const now = new Date(data.getTime() - (3 * 60 * 60 * 1000));
-        // Check if email exists
+        //check if email exists
         const emailExists = await Users.findOne({ 'info.email': email });
         if (emailExists) {
             return res.status(422).json({ error: 'E-mail já cadastrado!' });
@@ -57,45 +55,42 @@ router.post('/auth/user/sign-up', async (req: Request, res: Response, next: Next
     }
 });
 
-// Login users
+//Login users
 router.post('/auth/user/sign-in', async (req: Request, res: Response) => {
     const { info, security } = req.body;
     const { email } = info;
-    const { password } = security;
+    const { password } = security
 
     if (!email) {
-        return res.status(422).json({ message: 'O e-mail é obrigatório!' });
+        return res.status(422).json({ message: 'O e-mail é obrigatório!' })
     }
-    // Check if user exists
+    //check if user exists
     const user: IUsers | null = await Users.findOne({ 'info.email': email });
     if (!user) {
-        return res.status(404).json({ message: 'Usuário não cadastrado!' });
+        return res.status(404).json({ message: 'Usuário não cadastrado!' })
     }
-    // Check if password matches
-    const checkPassword: boolean = bcrypt.compareSync(password, user.security.password);
+    // check if password match
+    const checkPassword: boolean = await bcrypt.compare(password, user.security.password)
     if (!checkPassword) {
-        return res.status(422).json({ message: 'Senha inválida!' });
+        return res.status(422).json({ message: 'Senha invalida!' })
     }
     try {
         const secret: string | undefined = process.env.SECRET;
-        const token: string = jwt.sign(
-            {
-                id: user.info._id,
-            },
+        const token: string = jwt.sign({
+            id: user.info._id,
+        },
             secret || '',
         );
         res.status(200).json({
             message: 'Usuário logado com sucesso',
             token,
             user
-        });
+        })
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: 'Aconteceu um erro no servidor, tente novamente mais tarde!' });
     }
 });
-
-
 // Update - User
 router.patch('/user/update-by-id/:id', async (req: Request, res: Response) => {
     moment.locale('pt-BR');
