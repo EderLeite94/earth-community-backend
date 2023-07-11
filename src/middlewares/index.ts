71f92105a5f7ed67d18ac340c5c871941df97479
Leite <ederaparecido.leite@dxc.com>
2023-07-11 10:17:38 -0300

    cors

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,11 +2,11 @@ import cors from 'cors';
 import { RequestHandler } from 'express';
 
 function corsMiddleware(): RequestHandler {
-  const allowedOrigins = ['http://localhost:3000', process.env.ORIGIN_CLOUD];
+  const allowedOrigins = ['Access-Control-Allow-Origin', process.env.ORIGIN_CLOUD];
   const corsOptions = {
     origin: allowedOrigins,
     methods: ['POST'],
-    allowedHeaders: ['Access-Control-Allow-Origin','https://www.earthcommunity.com.br'],
+    allowedHeaders: ['Access-Control-Allow-Methods','GET, PUT, POST, DELETE, PATCH'],
   };
   return cors();
 }
