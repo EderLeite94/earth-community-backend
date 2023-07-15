af4f536a6cca092d3b8735fb8d47d23ac44b566b
Leite <ederaparecido.leite@dxc.com>
2023-07-15 02:26:26 -0300

    fix date

--- src/utils/date/index.ts
+++ src/utils/date/index.ts
@@ -1,8 +1,7 @@
 export const DateNow = () => {
-    // Get current date/time in Brazil timezone
-    const now = new Date(new Date().getTime());
-    return now;
-  };
-  
-  export const now = DateNow(); // Exporting the current date/time as well
-  
+  // Get current date/time in Brazil timezone
+  const now = new Date(new Date().getTime() - (3 * 60 * 60 * 1000));
+  return now;
+};
+
+export const now = DateNow(); // Exporting the current date/time as well
